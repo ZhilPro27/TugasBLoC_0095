23729ed837f10d55486abd9e2b9f1cb51652e5af
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-08 18:15:31 +0700

    Menambahkan kelas OrderSubmitted untuk mengelola pengiriman pesanan dalam OrderBloc.

--- lib/bloc/order/order_event.dart
+++ lib/bloc/order/order_event.dart
@@ -5,3 +5,20 @@ abstract class OrderEvent extends Equatable {
   List<Object?> get props => [];
 }
 
+class OrderSubmitted extends OrderEvent {
+  final String makanan;
+  final String minuman;
+  final int jumlahMakanan;
+  final int jumlahMinuman;
+
+  OrderSubmitted({
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+  });
+
+  @override
+  List<Object?> get props => [makanan, minuman, jumlahMakanan, jumlahMinuman];
+}
+
