6d8d91532c7dcc6905b3a535325cefb9c8c0af28
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-08 17:59:32 +0700

    Menambahkan kelas AuthBloc untuk mengelola logika autentikasi pengguna.

---
+++ lib/bloc/auth/auth_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<LoginSubmitted>(_onLoginSubmitted);
+    on<RegisterSubmitted>(_onRegisterSubmitted);
+    on<LogoutRequested>(_onLogoutRequested);
+  }
+
+  
+}
