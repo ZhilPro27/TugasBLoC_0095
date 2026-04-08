e17a34c40c70bf371f3807e6f8ff2cde4ac984ca
ZhilPro27 <zhilalkrisna@gmail.com>
2026-04-08 17:59:49 +0700

    Menambahkan logika untuk menangani proses login pada AuthBloc.

--- lib/bloc/auth/auth_bloc.dart
+++ lib/bloc/auth/auth_bloc.dart
@@ -11,5 +11,19 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     on<LogoutRequested>(_onLogoutRequested);
   }
 
+  Future<void> _onLoginSubmitted(
+    LoginSubmitted event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(AuthLoading());
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (event.email.isNotEmpty && event.password.isNotEmpty) {
+      emit(AuthAuthenticated(email: event.email));
+    } else {
+      emit(AuthError(message: 'Email atau password salah'));
+    }
+  }
+
   
 }
